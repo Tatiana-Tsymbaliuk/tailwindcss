55167e25872d16e96edc9eeca23c4615d04673ba
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2023-11-26 01:06:13 +0100

    create border-gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      gray: "#323232",
+      white: "#FFFFFF",
+      violet: "#8B60FA",
+    },
+    extend: {
+      borderColor: {
+        "gradient-violet-write": "linear-gradient(to bottom, #8B60FA, #FFFFFF)",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".border-gradient": {
+          "border-image": "linear-gradient(to bottom, #8B60FA, #323232) 1",
+          "border-image-slice": 1,
+          "border-radius": "0.5rem",
+        },
+      };
+
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
+};
