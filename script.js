f9f4106772ff97fe89aef550b5bbabb9df3d1daf
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2023-11-30 01:13:49 +0100

    add language menu

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-// function toggleModal() {
-//   var modal = document.getElementById("myModals");
-//   modal.style.display =
-//     modal.style.display === "none" || modal.style.display === ""
-//       ? "block"
-//       : "none";
-// }
+function toggleLanguage() {
+  var language = document.getElementById("myLanguage");
+  language.style.display =
+    language.style.display === "none" || language.style.display === ""
+      ? "block"
+      : "none";
+}
 function toggleModal() {
   var modal = document.getElementById("myModals");
   var btnIcon = document.getElementById("btnIcon");
 
   if (modal.style.display === "none" || modal.style.display === "") {
     modal.style.display = "block";
-    btnIcon.src = "./public/image/close-modal.svg"; // Поменяйте на свой путь к SVG для закрытой иконки
+    btnIcon.src = "./public/image/close-modal.svg";
   } else {
     modal.style.display = "none";
-    btnIcon.src = "./public/image/open-modal.svg"; // Поменяйте на свой путь к SVG для открытой иконки
+    btnIcon.src = "./public/image/open-modal.svg";
   }
 }
